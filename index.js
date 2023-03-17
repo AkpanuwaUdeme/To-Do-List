a0f6c7e7144803b6b9f3f2da534e86a8a0e0dc5b
AkpanuwaUdeme <akpanuwau@gmail.com>
2023-03-17 11:41:29 +0100

    edited

--- index.js
+++ index.js
@@ -1,31 +1,24 @@
-// Get the input field and the todo list
-const todoInput = document.getElementById("todo-input");
-const todoList = document.getElementById("todo-list");
+document.getElementById('add-btn').addEventListener('click', addTodo);
+document.getElementById('todo-input').addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        addTodo();
+    }
+});
+loadTodos();
 
-// Load the todo list from localStorage (if it exists)
-const todos = JSON.parse(localStorage.getItem("todos")) || [];
+function loadTodos() {
+    const todos = JSON.parse(localStorage.getItem('todos')) || [];
+    todos.forEach(todo => {
+        addTodoElement(todo);
+    });
+}
 
-// Render the todo list
-function renderTodoList() {
-  // Clear the current list items
-  todoList.innerHTML = "";
-
-  // Loop through the todos array and create new list items
-  for (let i = todos.length - 1; i >= 0; i--) {
-    const todo = todos[i];
-
-    const li = document.createElement("li");
-
-    const span = document.createElement("span");
-    span.innerText = todo;
-
-    const editBtn = document.createElement("button");
-    editBtn.innerText = "Edit";
-    editBtn.classList.add("edit");
-    editBtn.addEventListener("click", () => editTodoItem(i));
-
-    const deleteBtn = document.createElement("button");
-    deleteBtn.innerText = "Delete";
-    
-
-    }}
+function addTodo() {
+    const input = document.getElementById('todo-input');
+    const todo = input.value.trim();
+    if (todo) {
+        addTodoElement(todo);
+        saveTodo(todo);
+        input.value = '';
+    }
+  }
